6c05461c721917efc0492a0f7263a10fac24df36
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-05-10 13:52:45 +0200

    First induction step

--- mancala-endgames/src/main.rs
+++ mancala-endgames/src/main.rs
@@ -1,11 +1,26 @@
 use std::fmt;
 
 struct Mancala {
-    pub position: Vec<u16>,
+    pub position: Vec<u8>,
 }
 
 fn perfect_position(n: u8) -> Mancala {
-    Mancala { position: vec![1] }
+    if n == 1 {
+        Mancala { position: vec![1] }
+    } else {
+        let mut position: Vec<u8> = perfect_position(n - 1).position.to_vec();
+        position.push(n);
+        decrement_below(n, &mut position);
+        Mancala { position: position }
+    }
+}
+
+fn decrement_below(n: u8, position: &mut Vec<u8>) {
+    let mut index = n;
+    while index > (0 as u8) {
+        index -= 1;
+        position[index as usize] -= 1;
+    }
 }
 
 impl fmt::Display for Mancala {
@@ -19,7 +34,7 @@ impl fmt::Display for Mancala {
 }
 
 fn main() {
-    let endgame = perfect_position(1);
+    let endgame = perfect_position(2);
 
     println!("{}", endgame);
 }
