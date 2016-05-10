58e4a05a14840285eb1c0cab381ba9944e801686
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-05-10 12:21:19 +0200

    Create and display Mancala position

--- mancala-endgames/src/main.rs
+++ mancala-endgames/src/main.rs
@@ -1,3 +1,21 @@
+use std::fmt;
+
+struct Mancala {
+    pub position: Vec<i32>,
+}
+
+impl fmt::Display for Mancala {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[").unwrap();
+        for beans in &self.position {
+            write!(f, " {}", beans).unwrap();
+        }
+        write!(f, " ]")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let endgame = Mancala { position: vec![1, 2, 3, 4] };
+
+    println!("{}", endgame);
 }
