ea3926f47e315c75b34435c8616958d858b2ede2
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-05-10 14:49:57 +0200

    Test decrement below

--- mancala-endgames/src/main.rs
+++ mancala-endgames/src/main.rs
@@ -15,7 +15,7 @@ fn perfect_position(n: u8) -> Mancala {
     }
 }
 
-fn decrement_below(n: u8, position: &mut Vec<u8>) {
+pub fn decrement_below(n: u8, position: &mut Vec<u8>) {
     let mut index = n;
     while index > (0 as u8) {
         index -= 1;
@@ -37,4 +37,27 @@ fn main() {
     let endgame = perfect_position(2);
 
     println!("{}", endgame);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn decrement_below_should_work_correctly() {
+        let mut position = vec![1, 2, 3, 4];
+
+        decrement_below(2, &mut position);
+
+        assert_eq!(vec![0, 1, 3, 4], position);
+    }
+
+    #[test]
+    fn decrement_below_should_work_correctly_with_zero() {
+         let mut position = vec![1, 2, 3, 4];
+
+        decrement_below(0, &mut position);
+
+        assert_eq!(vec![1, 2, 3, 4], position);
+    }
 }
