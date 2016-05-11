8fdb9b650ff4e959394ae827d0751f1b9ef7640e
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-05-11 11:47:50 +0200

    Finish perfect_position algoritmh

--- mancala-endgames/src/main.rs
+++ mancala-endgames/src/main.rs
@@ -1,16 +1,17 @@
 use std::fmt;
 
-struct Mancala {
+pub struct Mancala {
     pub position: Vec<i32>,
 }
 
-fn perfect_position(n: i32) -> Mancala {
+pub fn perfect_position(n: i32) -> Mancala {
     if n == 0 {
         Mancala { position: vec![1] }
     } else {
         let mut position: Vec<i32> = perfect_position(n - 1).position.to_vec();
         position.push(n + 1);
         decrement_below(n, &mut position);
+        fill_zeros_below(n, &mut position);
         Mancala { position: position }
     }
 }
@@ -20,6 +21,37 @@ pub fn decrement_below(n: i32, position: &mut Vec<i32>) {
     while index > 0  {
         index -= 1;
         position[index as usize] -= 1;
+    }
+}
+
+pub fn fill_zeros_below(n: i32, position: &mut Vec<i32>) {
+    let mut index = 0;
+    while index < n {
+        if position[index as usize] == 0 {
+            fill_zero_at(index, position);
+        }
+        index += 1;
+    }
+}
+
+pub fn fill_zero_at(index: i32, position: &mut Vec<i32>) {
+    if position[index as usize] != 0 { panic!("position at index is not zero"); }
+    if index == 0 {
+        let mut prefix_index = 0;
+        while position[prefix_index as usize] == 0 {
+            prefix_index += 1;
+        }
+        prefix_index -= 1;
+        let prefix: Vec<i32> = perfect_position(prefix_index).position.to_vec();
+        while prefix_index >= 0 {
+            let i = prefix_index as usize;
+            position[i] = prefix[i];
+            prefix_index -= 1;
+        }
+    } else {
+        position[index as usize] = index + 1;
+        decrement_below(index, position);
+        fill_zeros_below(index, position);
     }
 }
 
@@ -60,4 +92,33 @@ mod tests {
 
         assert_eq!(vec![1, 2, 3, 4], position);
     }
+
+    #[test]
+    fn fill_zero_at_should_work_correctly_with_zero_not_in_first_place() {
+        let mut position = vec![2, 0, 3];
+
+        fill_zero_at(1, &mut position);
+
+        assert_eq!(vec![1, 2, 3], position);
+    }
+
+   #[test]
+    fn fill_zero_at_should_work_correctly_with_zero_in_first_place() {
+        let mut position = vec![0, 1, 3];
+
+        fill_zero_at(0, &mut position);
+
+        assert_eq!(vec![1, 1, 3], position);
+    }
+
+    #[test]
+    fn perfect_position_should_work_correctly() {
+        assert_eq!(vec![1], perfect_position(0).position.to_vec());
+
+        assert_eq!(vec![1, 2], perfect_position(1).position.to_vec());
+
+        assert_eq!(vec![1, 1, 3], perfect_position(2).position.to_vec());
+
+        assert_eq!(vec![1, 2, 2, 4], perfect_position(3).position.to_vec());
+   }
 }
