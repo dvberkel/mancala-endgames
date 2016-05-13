c10e80fed8a5c9a5932cef4f7f4214dbbe877108
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-05-13 10:23:14 +0200

    Introduce helper functions

--- mancala-endgames/src/mancala/mod.rs
+++ mancala-endgames/src/mancala/mod.rs
@@ -47,21 +47,30 @@ pub fn fill_zeros_below(n: i32, position: &mut Vec<i32>) {
 pub fn fill_zero_at(index: i32, position: &mut Vec<i32>) {
     if position[index as usize] != 0 { panic!("position at index is not zero"); }
     if index == 0 {
-        let mut prefix_index = 0;
-        while position[prefix_index as usize] == 0 {
-            prefix_index += 1;
-        }
-        prefix_index -= 1;
+        let prefix_index = zero_prefix_length(position) - 1;
         let prefix: Vec<i32> = perfect_game(prefix_index).position.to_vec();
-        while prefix_index >= 0 {
-            let i = prefix_index as usize;
-            position[i] = prefix[i];
-            prefix_index -= 1;
-        }
+        copy_prefix(& prefix, position);
     } else {
         position[index as usize] = index + 1;
         decrement_below(index, position);
         fill_zeros_below(index, position);
+    }
+}
+
+fn zero_prefix_length(position: &Vec<i32>) -> i32 {
+    let mut prefix_length = 0;
+    while position[prefix_length as usize] == 0 {
+        prefix_length += 1;
+    }
+    prefix_length
+}
+
+fn copy_prefix(prefix: &Vec<i32>, position: &mut Vec<i32>) {
+    let mut prefix_index: i32 = (prefix.len() as i32) - 1;
+    while prefix_index >= 0 {
+        let i = prefix_index as usize;
+        position[i] = prefix[i];
+        prefix_index -= 1;
     }
 }
 
